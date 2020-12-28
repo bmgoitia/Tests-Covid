e0c853c4cfc122970ea5559a2a0896a3e96c66f1
begomart <bmgoitia@gmail.com>
2020-12-28 16:41:53 +0100

    v1

---
+++ README.md
@@ -0,0 +1,1 @@
+![US election 2020](./img/testCovid.png?raw=true "US 2020 election")
---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+
+
+
+$('.testText').hide();
+
+$('.test').click(function(){
+    $('.testImg,.testText', this).toggle("slow");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
